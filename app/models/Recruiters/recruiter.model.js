a7398208719740b2abc5ceb094a63a307df73aa8
Jeff930 <jefferson30.jdg@gmail.com>
2020-09-14 16:06:30 +0800

    Created model for recruiters

---
+++ app/models/Recruiters/recruiter.model.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose')
+
+const recruiterSchema = new Schema({
+    email: {
+        type: String,
+        required: (true, 'Email is required.'),
+        unique: (true, 'Email already exists.')
+    },
+    userName: {
+        type: String,
+        required: (true, 'User name is required.'),
+        unique: (true, 'User name is already taken.')
+    },
+    password: {
+        type: String,
+        required: (true, 'Password is required.')
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    phone: {
+        type: String,
+        default: ''
+    },
+    on2FA: {
+        type: Boolean,
+        default: false
+    },
+    registrationTag: {
+        type: String
+    },
+    metadata: {
+        verificationToken: {
+            type: String
+        },
+        hasProfile: {
+            type: Boolean,
+            default: false
+        },
+        organization: {
+            type: Schema.Types.ObjectId, ref: 'organization',
+        },
+        dateCreated: Date,
+        dateUpdated: Date
+    }
+})
+
+module.exports = RecruiterModel = onphpartnersdb.model('recruiter', recruiterSchema);
