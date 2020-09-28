b7ff4b4af1dbf789dcedaab2a19fb1fa39e9b2f5
Jeff930 <jefferson30.jdg@gmail.com>
2020-09-28 16:59:06 +0800

    Created company model

---
+++ app/models/Company/company.model.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose')
+
+const companySchema = new Schema({
+    email: {
+        type: String,
+        required: (true, 'Email is required.'),
+        unique: (true, 'Email already exists.')
+    },
+    companyName: {
+        type: String,
+        required: (true, 'Company name is required.')
+    },
+    description: {
+        type: String,
+        required: (true, 'Company description is required.')
+    },
+    location: {
+        type: String,
+        required: (true, 'Company location is required.')
+    },
+    companySize: {
+        type: String,
+        required: (true, 'Company size is required.')
+    },
+    dressCode: {
+        type: String,
+        required: (true, 'Company size is required.')
+    },
+    benefits: {
+        type: String,
+        required: (true, 'Company size is required.')
+    },
+    workHours: {
+        type: String,
+        required: (true, 'Company size is required.')
+    },
+    spokenLanguage: {
+        type: String,
+        required: (true, 'Company size is required.')
+    }
+})
+
+module.exports = CompanyModel = onphdbpartners.model('company', companySchema);
